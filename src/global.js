a9de65a1b6d9b4b52d1a2905421c458d06630cc1
Lenin Compres <lenincompres@gmail.com>
2025-06-07 12:29:11 -0400

    update

--- src/global.js
+++ src/global.js
@@ -1,7 +1,9 @@
 const QS = DOM.querystring();
 export let rgb = QS.rgb ? `#${QS.rgb}` : undefined;
 export let fav = QS.fav ? `#${QS.fav}` : undefined;
-export let randRGB = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)];
+
+const cRandom = (range = 0.5, total = 255) => Math.floor(total * range * (0.5 + Math.random()));
+export let randRGB = [cRandom(), cRandom(), cRandom()];
 export let rand = `#${randRGB.map(v => v.toString(16).padStart(2, "0")).join('')}`;
 
 export default rgb;
